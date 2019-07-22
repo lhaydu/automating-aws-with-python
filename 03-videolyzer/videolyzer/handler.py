1f6943aeb4a864b42831f474c8c86bd1263067ef
lhaydu <lhaydu@troweprice.com>
2019-07-22 18:35:25 -0400

    Get Video Label Data from Rekognition

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -3,6 +3,28 @@ import urllib
 import boto3
 import os
 
+#Helper Functions
+def get_video_labels(job_id):
+    rekognition_client = boto3.client('rekognition')
+
+    response = rekognition_client.get_label_detection(JobId=job_id)
+
+    next_token = response.get('NextToken', None)
+
+    while next_token:
+        next_page = rekognition_client.get_label_detection(
+            JobId=job_id,
+            NextToken=next_token
+        )
+        next_token = next_page.get('NextToken', None)
+
+        response['Labels'].extend(next_page['Labels'])
+
+    return response
+
+def put_labels_in_db(data, video_name, video_bucket):
+    pass
+
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
     response = rekognition_client.start_label_detection(
@@ -19,23 +41,30 @@ def start_label_detection(bucket, key):
 
     print(response)
     return
-    #job_id = response['JobId']
-    #result = rekognition_client.get_label_detection(JobId=job_id)
 
+
+#Lambda Events
 def start_processing_video(event, context):
+
     for record in event['Records']:
         bucket_name = record['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(record['s3']['object']['key'])
 
         start_label_detection(bucket_name, key)
 
-    # response = rekognition_client.start_label_detection(Video={'S3Object': { 'Bucket': bucket.name, 'Name': path.name}})
-    # job_id = response['JobId']
-    # result = rekognition_client.get_label_detection(JobId=job_id)
-
     return
 
 def handle_label_detection(event, context):
+    for record in event['Records']:
 
-    print(event)
+        message = json.loads(record['Sns']['Message'])
+        job_id = message['JobId']
+
+        s3_object = message['Video']['S3ObjectName']
+        s3_bucket = message['Video']['S3Bucket']
+
+        response = get_video_labels(job_id)
+        
+        put_labels_in_db(response, s3_object, s3_bucket)
+
     return
