d08f898e1fc5b36406f6062919b6435402f99eb8
lhaydu <lhaydu@troweprice.com>
2019-07-22 13:20:16 -0400

    Trigger rekognition job in lambda function

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,6 +1,30 @@
 import json
+import urllib
+import boto3
 
+def start_label_detection(bucket, key):
+    rekognition_client = boto3.client('rekognition')
+    response = rekognition_client.start_label_detection(
+        Video={'S3Object': {
+            'Bucket': bucket,
+            'Name': key
+            }
+        })
+
+    print(response)
+    return
+    #job_id = response['JobId']
+    #result = rekognition_client.get_label_detection(JobId=job_id)
 
 def start_processing_video(event, context):
-    print(event)
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+
+        start_label_detection(bucket_name, key )
+
+    # response = rekognition_client.start_label_detection(Video={'S3Object': { 'Bucket': bucket.name, 'Name': path.name}})
+    # job_id = response['JobId']
+    # result = rekognition_client.get_label_detection(JobId=job_id)
+
     return
